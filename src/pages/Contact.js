eadb9afae2f069974d679fa99a3819494862e935
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-09-15 15:14:18 -0400

    Updated Contacts Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import NavBar from '../components/Navbar/NavBar';
 import Footer from '../components/Footer';
-import {useDocTitle} from '../components/CustomHook';
-import axios from 'axios';
-// import emailjs from 'emailjs-com';
-import Notiflix from 'notiflix';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
@@ -14,6 +11,7 @@ const Contact = () => {
     const [phone, setPhone] = useState('')
     const [message, setMessage] = useState('')
     const [errors, setErrors] = useState([])
+    const form = useRef();
 
     const clearErrors = () => {
         setErrors([])
@@ -28,52 +26,21 @@ const Contact = () => {
     }
 
     const sendEmail = (e) => {
-        e.preventDefault();
-        document.getElementById('submitBtn').disabled = true;
-        document.getElementById('submitBtn').innerHTML = 'Loading...';
-        let fData = new FormData();
-        fData.append('first_name', firstName)
-        fData.append('last_name', lastName)
-        fData.append('email', email)
-        fData.append('phone_number', phone)
-        fData.append('message', message)
+    e.preventDefault();
 
-        axios({
-            method: "post",
-            url: process.env.REACT_APP_CONTACT_API,
-            data: fData,
-            headers: {
-                'Content-Type':  'multipart/form-data'
-            }
-        })
-        .then(function (response) {
-            document.getElementById('submitBtn').disabled = false;
-            document.getElementById('submitBtn').innerHTML = 'send message';
-            clearInput()
-            //handle success
-            Notiflix.Report.success(
-                'Success',
-                response.data.message,
-                'Okay',
-            );
-        })
-        .catch(function (error) {
-            document.getElementById('submitBtn').disabled = false;
-            document.getElementById('submitBtn').innerHTML = 'send message';
-            //handle error
-            const { response } = error;
-            if(response.status === 500) {
-                Notiflix.Report.failure(
-                    'An error occurred',
-                    response.data.message,
-                    'Okay',
-                );
-            }
-            if(response.data.errors !== null) {
-                setErrors(response.data.errors)
-            }
-            
-        });
+    emailjs
+      .sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_EMAIL_ID, form.current, {
+        publicKey: process.env.REACT_APP_EMAIL_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          clearInput()
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
     }
     return (
         <>
@@ -83,7 +50,7 @@ const Contact = () => {
             <div id='contact' className="flex justify-center items-center mt-8 w-full bg-white py-12 lg:py-24 ">
                 <div className="container mx-auto my-8 px-4 lg:px-20" data-aos="zoom-in">
 
-                <form onSubmit={sendEmail}>
+                <form ref={form} onSubmit={sendEmail}>
 
                     <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-5/5 lg:pl-20 lg:pr-50 mr-auto rounded-2xl shadow-2xl">
                         <div className="flex">
@@ -92,7 +59,7 @@ const Contact = () => {
                         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
                                 <div>
                                     <input 
-                                        name="first_name" 
+                                        name="user_name" 
                                         className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                         type="text" 
                                         placeholder="First Name*" 
@@ -122,7 +89,7 @@ const Contact = () => {
 
                                 <div>
                                     <input 
-                                        name="email"
+                                        name="user_email"
                                         className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                         type="email" 
                                         placeholder="Email*"
@@ -163,7 +130,7 @@ const Contact = () => {
                                 <p className="text-red-500 text-sm">{errors.message}</p>
                             }
                         </div>
-                        <div className="my-2 w-1/2 lg:w-2/4">
+                        <div className="my-2 w-4/4 lg:w-4/4">
                             <button type="submit" id="submitBtn" className="uppercase text-sm font-bold tracking-wide bg-gray-500 hover:bg-red-900 text-gray-100 p-3 rounded-lg w-full 
                                     focus:outline-none focus:shadow-outline">
                                 Send Message
